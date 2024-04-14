c67ef90c297cc7bcf457d90ef0f3f3c00e69d3f2
kalu jonah chinonso <kaluchinonso18@gmail.com>
2024-04-14 08:28:30 -0700

    A function that checks for lowercase character.

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - KALU JONAH CHINONSO
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
