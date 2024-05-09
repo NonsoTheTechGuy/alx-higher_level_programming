a3629952b709433253bfc9734e26387e676a3a8f
kalu jonah chinonso <kaluchinonso18@gmail.com>
2024-05-09 13:52:06 -0700

    A class Student that defines a student by Public instance attributes: first_name, last_name, age

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class Student"""
+
+"""
+Student class Module
+Public instance attributes:
+first_name
+last_name
+age
+Instantiation with first_name, last_name and age:
+def __init__(self, first_name, last_name, age):
+Public method def to_json(self): that retrieves a dictionary representation
+of a Student instance (same as 8-class_to_json.py)
+You are not allowed to import any module
+"""
 
 
 class Student:
     """
-    Represent a student.
+    Class that defines a Student
     """
-
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
+        """ Initialize the class objects
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -16,6 +27,6 @@ class Student:
 
     def to_json(self):
         """
-        Gets a dictionary representation of the Student
+        retrieve dictionary representation of student
         """
-        return self.__dict___
+        return self.__dict__
